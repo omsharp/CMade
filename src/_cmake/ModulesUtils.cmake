393db758110bb8e0ba2f40f2da61c7f9af5d245a
Omar Rwemi <omsharp@gmail.com>
2024-04-25 21:53:30 +0200

    Organise CMake code.

---
+++ src/_cmake/ModulesUtils.cmake
@@ -0,0 +1,27 @@
+# -------------------------------------------------#
+# --------- utilites for managing modules ---------#
+# -------------------------------------------------#
+
+# a property that holds a list of all modules in project
+set_property(GLOBAL PROPERTY modules_list)
+
+# adds the source files as a static library.
+# using the caller's directory as a module name.
+function(add_module_static)
+    # extract the current directory name into module_name to be used as module's name
+    cmake_path(GET CMAKE_CURRENT_LIST_DIR FILENAME module_name)
+    string(REPLACE " " "_" module_name ${module_name})
+
+    # add module as a static library using its source files
+    add_library(${module_name} STATIC
+        ${ARGV}
+    )
+
+    # include directory (current dir of this file)
+    target_include_directories(${module_name} PUBLIC ${CMAKE_CURRENT_LIST_DIR})
+
+    # add module to the modules list property
+    get_property(temp GLOBAL PROPERTY modules_list)
+    list(APPEND temp ${module_name})
+    set_property(GLOBAL PROPERTY modules_list ${temp})
+endfunction(add_module_static)
